28aa224bb8f915a3ae2d6e67d564eb147de1c2ef
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-11-23 10:39:02 +0600

    setup project with eslint and mongoose

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config";
+
+
+
+async function main() {
+
+    try {
+
+        await mongoose.connect(config.database_url as string);
+        app.listen(config.port, () => {
+            console.log(`server running on port : ${config.port}`);
+        })
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+}
+
+main();
+
